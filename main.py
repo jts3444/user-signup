ea844c10fcbb2ade0b0b0a337e46b15e8a8a0b78
jts3444 <jts3444@hotmail.com>
2018-08-31 18:04:00 -0700

    fixed error message issue, stuck on functions

--- main.py
+++ main.py
@@ -14,18 +14,31 @@ def display_home():
     return render_template('home.html')
 
 @app.route("/home", methods=['POST'])
+
+def valid_length(userinfo):
+    if len(userinfo) > 20 or len(userinfo) < 3:
+        return False
+    return True
+
+def valid_content(userinfo):
+    if ' ' in userinfo:
+        return False
+    return True
+
 def validate_usersignup():
+# checking username   
     username = request.form['username']
 
     username_error = ''
 
-    if len(username) > 20 or len(username) < 3:
+    if valid_length(username) == False:
         username_error = 'Username length must be between 3 and 20 characters'
         username = ''
-    elif ' ' in username:
+    elif valid_content(username) == False:
         username_error = 'Username cannot contain any spaces'
         username = ''
 
+# setting up password, checking if valid
     password = request.form['password']
 
     password_error = ''
@@ -37,7 +50,7 @@ def validate_usersignup():
         password_error = 'Password cannot contain any spaces'
         password = ''
 
-    password = request.form['password']
+# checking verify password input
     verify_password = request.form['verify_password']
 
     verify_password_error = ''
@@ -46,9 +59,16 @@ def validate_usersignup():
         verify_password_error = 'Passwords do not match'
         verify_password = ''
 
+# email verification
     email = request.form['email']
+    email_error = ''
 
-    if not username_error and not password_error and not verify_password_error:
+    if email == True:
+        if ['@', '.'] not in email:
+            email_error = "Not a valid e-mail address"
+            email = ''
+
+    if not username_error and not password_error and not verify_password_error and not email_error:
         return redirect('/welcome?username={0}'.format(username))
 
     else:
